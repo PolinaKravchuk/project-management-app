ae4c1d3a5526b721f52543a1dbb9d27721b97cad
PolinaKravchuk <polina8kravchuk@gmail.com>
2022-11-16 10:23:19 +0400

    feat: add Constants file

---
+++ src/utils/Constants.tsx
@@ -0,0 +1,14 @@
+const Constants = {
+  HEADER_HEIGHT: 200,
+
+  FORM_TYPE: {
+    LOGIN: 'login',
+    REGISTRATION: 'registration',
+  },
+
+  LANGUAGE: {
+    RU: 'RU',
+    EN: 'EN',
+  },
+};
+export default Constants;
